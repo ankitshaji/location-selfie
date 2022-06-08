99652e521fbae2b5787ed9373af20512eb1d08b0
Ankit Shaji <ankitshaji@gmail.com>
2022-06-09 01:17:09 +1200

    serverside - accept post request,send json response back

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+//server
 const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("listning at 3000"));
-app.use(express.static('public'))
+
+//server understands static and json
+app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+
+app.post("/api", (req, res) => {
+  console.log("Request received");
+  console.log(req.body);
+  res.json({
+    status: "Success",
+    latitude: req.body.lat,
+    longitude: req.body.lon,
+  });
+});
