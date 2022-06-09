ee28454afa6d4adc265349af2aff50c68ca91a65
Ankit Shaji <ankitshaji@gmail.com>
2022-06-09 22:19:32 +1200

    nedb database -create/load/save

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 //server
+//npm install express
+//npm install nedb
+const Datastore = require("nedb");
 const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("listning at 3000"));
@@ -7,11 +10,19 @@ app.listen(3000, () => console.log("listning at 3000"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
+//create database
+const database = new Datastore("database.db");
+database.loadDatabase();
+
+//server accepts post requests
 app.post("/api", (req, res) => {
   console.log("Request received");
-  console.log(req.body);
+  const timestamp = Date.now();
+  req.body.timestamp = timestamp;
+  database.insert(req.body);
   res.json({
     status: "Success",
+    timestamp: req.body.timestamp,
     latitude: req.body.lat,
     longitude: req.body.lon,
   });
